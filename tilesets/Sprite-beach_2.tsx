31e39ddd675a9da841d6760cf70c0213eed34754
Katrina0104 <121471979+Katrina0104@users.noreply.github.com>
2025-10-31 14:57:05 +0800

    Add files via upload

--- tilesets/Sprite-beach_2.tsx
+++ tilesets/Sprite-beach_2.tsx
@@ -2,4 +2,3 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Sprite-beach(2)" tilewidth="32" tileheight="32" tilecount="1" columns="1">
  <image source="Sprite-beach(2).png" width="32" height="32"/>
 </tileset>
-
